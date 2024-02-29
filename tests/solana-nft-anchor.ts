c0ed2a7eae8b4914fa09f616353739ee6daf3c73
Machiinnar <enoiniodungide20@gmail.com>
2024-02-29 12:53:28 +0100

    solana-nft-anchor

---
+++ tests/solana-nft-anchor.ts
@@ -0,0 +1,160 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaNftAnchor } from "../target/types/solana_nft_anchor";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import {
+  findMasterEditionPda,
+  findMetadataPda,
+  mplTokenMetadata,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
+
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+describe("solana-nft-anchor", async () => {
+  // Configured the client to use the devnet cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace
+    .SolanaNftAnchor as Program<SolanaNftAnchor>;
+
+  const signer = provider.wallet;
+
+  const umi = createUmi("https://api.devnet.solana.com")
+    .use(walletAdapterIdentity(signer))
+    .use(mplTokenMetadata());
+
+  const mint = anchor.web3.Keypair.generate();
+
+  // Derive the associated token address account for the mint
+  const associatedTokenAccount = await getAssociatedTokenAddress(
+    mint.publicKey,
+    signer.publicKey
+  );
+
+  // derive the metadata account
+  let metadataAccount = findMetadataPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  //derive the master edition pda
+  let masterEditionAccount = findMasterEditionPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  const metadata = {
+    name: "Kobeni",
+    symbol: "kBN",
+    uri: "https://raw.githubusercontent.com/687c/solana-nft-native-client/main/metadata.json",
+  };
+
+  it("mints nft!", async () => {
+    const tx = await program.methods
+      .initNft(metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        signer: provider.publicKey,
+        mint: mint.publicKey,
+        associatedTokenAccount,
+        metadataAccount,
+        masterEditionAccount,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenMetadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .signers([mint])
+      .rpc();
+
+    console.log(
+      `mint nft tx: https://explorer.solana.com/tx/${tx}?cluster=devnet`
+    );
+    console.log(
+      `minted nft: https://explorer.solana.com/address/${mint.publicKey}?cluster=devnet`
+    );
+  });
+});
+
+
+# Solana-NFT-Anchor
+
+Based on the [Mint Your First NFT](https://calyptus.co/lessons/mint-your-first-nft/) lesson, this project shows how to mint your first NFT on the Solana blockchain using [Anchor](https://www.anchor-lang.com/) and [Metaplex](https://www.anchor-lang.com/).
+
+## Table of Contents
+- [Getting Started](#getting-started)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
+
+## Getting Started
+
+To use this fork, you need to have [yarn](https://yarnpkg.com/getting-started/install), [Anchor](https://www.anchor-lang.com/docs/installation) and the [Solana cli suite](https://solana.com/developers/guides/getstarted/setup-local-development) installed on your machine. 
+
+It is highly recommended that you start this project from scratch, following along with the tutorial. 
+
+To use the fork, follow the steps outlined below: 
+
+1. Clone your forked repo.
+
+```bash
+git clone https://github.com/<YOUR-USERNAME>/solana-nft-anchor
+```
+
+2. Change directory into the root of your cloned repo and install missing node packages
+
+```bash
+yarn install
+```
+
+**NOTE:** You must use yarn to install the dependencies. If you use a different package manager, you will run into issues minting the NFT.
+
+3. Build your anchor project.
+
+```bash
+anchor build
+```
+
+4. List the project deployment keys and copy the address to a clipboard
+
+```bash
+anchor keys list
+```
+
+5. Update your [`Anchor.toml`](Anchor.toml) file, by using the address generated in the previous step. 
+
+```toml
+[programs.devnet]
+solana_nft_anchor = "<ADD YOUR ADDRESS HERE>"
+```
+
+6. Update your [`lib.rs`](programs/solana-nft-anchor/src//lib.rs) file by adding the the address generated in step 4 to the `declare_id!()` macro
+
+```rust
+    // snip
+use mpl_token_metadata::{
+    pda::{find_master_edition_account, find_metadata_account},
+    state::DataV2,
+};
+
+declare_id!("<UPDATE HERE>");
+#[program]
+pub mod solana_nft_anchor {
+    // snip
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Questions
+
+Did you encounter a challenge following the tutorial or running the fork? 
+Head over to our [learning support](https://discord.com/channels/1130457754826461216/1132978998155165806) channel on our [Discord](https://discord.gg/38KftAhW) or alternatively, raise a ticket. 
+
+We are always happy to lend a helping hand
